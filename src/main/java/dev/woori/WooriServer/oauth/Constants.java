b37522dc22df5ce4129ca0e4be1f3fff390f235b
Jeyong <dlwpdyd201@naver.com>
2025-09-09 16:09:15 +0900

    [feat] AuthServer와 Feign 요청

---
+++ src/main/java/dev/woori/WooriServer/oauth/Constants.java
@@ -0,0 +1,10 @@
+package dev.woori.WooriServer.oauth;
+
+public abstract class Constants {
+    public static final String GRANT_TYPE = "grant_type";
+    public static final String CLIENT_ID = "client_id";
+    public static final String CLIENT_SECRET = "client_secret";
+    public static final String REDIRECT_URL = "redirect_uri";
+    public static final String CODE = "code";
+    public static final String AUTH_HEADER = "Authorization";
+}
