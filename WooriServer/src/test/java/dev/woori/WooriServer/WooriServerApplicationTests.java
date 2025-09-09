deaad3125a84594d4c74e48f001eaf69c31d234f
Jeyong <dlwpdyd201@naver.com>
2025-09-09 14:56:27 +0900

    프로젝트 수정

---
+++ WooriServer/src/test/java/dev/woori/WooriServer/WooriServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.woori.WooriServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooriServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
